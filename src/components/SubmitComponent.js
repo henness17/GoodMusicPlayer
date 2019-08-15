6e5d1d6130d8b08be8bb12c3f98ddda37ca00499
Ryan Henness <ryanhenness@gmail.com>
2019-08-15 14:27:14 -0700

    Cleanup.

--- src/components/SubmitComponent.js
+++ src/components/SubmitComponent.js
@@ -8,49 +8,49 @@ class SubmitComponent extends React.Component {
   render() {
     return (
       <div>
-      <form onSubmit={this.props.handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          value={this.props.title}
-          onChange={this.props.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="artist"
-          placeholder="Artist"
-          value={this.props.artist}
-          onChange={this.props.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="genre"
-          placeholder="Genre"
-          value={this.props.genre}
-          onChange={this.props.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="url"
-          placeholder="Url"
-          value={this.props.url}
-          onChange={this.props.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          name="artwork_url"
-          placeholder="Artwork Url"
-          value={this.props.artwork_url}
-          onChange={this.props.handleChange}
-        />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
+        <form onSubmit={this.props.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            value={this.props.title}
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="artist"
+            placeholder="Artist"
+            value={this.props.artist}
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="genre"
+            placeholder="Genre"
+            value={this.props.genre}
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="url"
+            placeholder="Url"
+            value={this.props.url}
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <input
+            type="text"
+            name="artwork_url"
+            placeholder="Artwork Url"
+            value={this.props.artwork_url}
+            onChange={this.props.handleChange}
+          />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
